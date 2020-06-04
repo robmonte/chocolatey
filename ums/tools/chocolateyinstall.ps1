3b2f60910ff2c8e5c845ffce27100e2b91261f8a
Robert Montefusco <robert.t.montefusco@utexas.edu>
2020-06-04 18:23:22 -0500

    Update version to 9.5.0

--- ums/tools/chocolateyinstall.ps1
+++ ums/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = 'ums'
 $installerType = 'exe' 
 $silentArgs = '/S'
-$url = 'https://sourceforge.net/projects/unimediaserver/files/9.4.3/UMS-9.4.3.exe/download'
-$checksum = '2A8594847C8627D3364D153C9B97BA24061802E98366B1C285A0F96E5E8B60C1'
+$url = 'https://sourceforge.net/projects/unimediaserver/files/9.5.0/UMS-9.5.0.exe/download'
+$checksum = 'F0A4E861281546FA37E8B8A1CBC4C3DF0EDBCC28D8CEAF84192D4F29C1C49BB9'
 $checksumType = 'sha256'
 $validExitCodes = @(0)
  
